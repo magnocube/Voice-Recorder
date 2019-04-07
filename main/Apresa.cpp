e9194d92dc0a6dc076e6ddbeb4f4483bdca09a98
Stefan <stefangroenendijk1998@hotmail.com>
2019-04-07 20:15:42 +0200

    implemented TCP protocol in apresa class

--- main/Apresa.cpp
+++ main/Apresa.cpp
@@ -1,13 +1,97 @@
 #include "Apresa.h"
+#define TAG "apresaTunnelTest"
 
-
-Apresa::Apresa(){
-
+Apresa::Apresa(){  //normal constructor
+    fileName = (char*)malloc(9 +20);  //the 20 is for the path that needs to be added, the 9 is for the file path length
 }
 
-void Apresa::setup(){
+void Apresa::setup(){  //for setting up any other things
 
 }
-void Apresa::setFileName(char* name){
+void Apresa::setFileName(char* name){  //will set the name used by the "sendFile()" function
     
+}
+void Apresa::sendFile(){
+    	
+	//char rx_buffer[128];
+    
+
+    //assuming nothing will be received
+    
+    connectTCP();
+    if(err == 0) {  //no problems
+        err = send(sock, payload, strlen(payload), 0);
+        ESP_LOGI(TAG, "succesfuls packet send");
+        if (err < 0) {
+            ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
+        }
+    }
+    disconnectTCP();
+    
+
+
+    
+
+    
+    
+
+    // int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+    // // Error occured during receiving
+    // if (len < 0) {
+    //     ESP_LOGE(TAG, "recv failed: errno %d", errno);
+    //     break;
+    // }
+    // // Data received
+    // else {
+    //     rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+    //     ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+    //     ESP_LOGI(TAG, "%s", rx_buffer);
+    // }
+
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    
+
+       
+     
+    
+}
+void Apresa::startSending(){
+    isSendingAFile = true;
+}
+bool Apresa::isSending(){
+    return isSendingAFile;
+}
+void Apresa::connectTCP(){  //using tcp protocol
+    char addr_str[128];
+    int addr_family;
+    int ip_protocol;
+
+    struct sockaddr_in destAddr;
+    destAddr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);    //TODO: make this into a variable
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(PORT);                        //TODO: make this into a variable
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+    inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
+
+    sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        return;
+    }
+    ESP_LOGI(TAG, "Socket created");
+
+    err = connect(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+        return;
+    }
+    ESP_LOGI(TAG, "Successfully connected");
+}
+void Apresa::disconnectTCP(){
+    if (sock != -1) {
+        ESP_LOGE(TAG, "Shutting down socket ");
+        shutdown(sock, 0);
+        close(sock);
+    }
 }
--- main/Apresa.h
+++ main/Apresa.h
@@ -1,15 +1,41 @@
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+#include "esp_log.h"
+#include "stdlib.h"
+#include "string.h"
+
 #pragma once
+#include <stdlib.h>
+
+
+
+
+#define HOST_IP_ADDR "192.168.2.32" //must be placed in a variable from settings.txt
+#define PORT 2016					 // ""
+
+
+static const char *payload = "Message from ESP32 ";
+
 
 class Apresa{    
     public:
         Apresa();
         void setup();                    //setup of the socket( for when connected to wifi/ethernet)
-        void sendFile();                 //sends an actual file (blocking)
-        bool startSending;               //to indicate that it can start sending the file (so an other thread can send it)
+        void sendFile();                 //sends an actual file (blocking, dont call fron task that should not be blocked, use: "startSending")
+        void startSending();               //to indicate that it can start sending the file (so an other thread can send it), non blocking for the thread calling it
+        bool isSending();                  //to let other tasks know that there is an active connection, will be used by apresa thread to start the connection
         void setFileName(char* name);    //set the name of the file to send (might also happen internal in this class)
 
     private:
+        void connectTCP();
+        void disconnectTCP();
         char * fileName;                 // malloc in main
+        bool isSendingAFile;
+
+        int sock;
+        int err;
     
 
 };
